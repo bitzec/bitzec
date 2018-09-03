320efab2574420a93ad1d02404495226107b1eac
bitzec <bitzec18@gmail.com>
2018-09-03 16:46:06 +0200

    rpc mining

--- src/main.cpp
+++ src/main.cpp
@@ -1711,7 +1711,7 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex)
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    CAmount nSubsidy = 12.5 * COIN;
+    CAmount nSubsidy = 15000 * COIN;
 
     // Mining slow start
     // The subsidy is ramped up linearly, skipping the middle payout of
