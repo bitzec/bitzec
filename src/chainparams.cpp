c4db520a07836458a0271150cdff567d7f22988c
bitzec <bitzec18@gmail.com>
2018-09-17 05:43:36 +0200

    sapling active

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,7 +51,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 
  /**
  *  >>> from hashlib import blake2s
- *  >>> 'Bitzec' + blake2s(b'the binary digit zero knowledge electronic currency').hexdigest()
+ *  >>> Bitzec ("the binary digit zero knowledge electronic currency')
  *
  * CBlock(hash=00040fe8, ver=4, hashPrevBlock=00000000000000, hashMerkleRoot=c4eaa5, nTime=1536721921, nBits=1f07ffff, nNonce=4695, vtx=1)
  *   CTransaction(hash=c4eaa5, ver=1, vin.size=1, vout.size=1, nLockTime=0)
@@ -106,8 +106,8 @@ public:
             Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
         //consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 1;
         //consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight = 20000;
+        //consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight = 0;
 
         consensus.fPowNoRetargeting=false;
         consensus.nLWMAHeight=10000;
@@ -284,16 +284,16 @@ public:
         consensus.nPowMaxAdjustDown = 32; // 32% adjustment down
         consensus.nPowMaxAdjustUp = 16; // 16% adjustment up
         consensus.nPowTargetSpacing = 2.5 * 60;
-        consensus.vUpgrades[Consensus::BASE_SPROUT].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::BASE_SPROUT].nActivationHeight =
+        //consensus.vUpgrades[Consensus::BASE_SPROUT].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::BASE_SPROUT].nActivationHeight =
             Consensus::NetworkUpgrade::ALWAYS_ACTIVE;
-        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
+        //consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
             Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight = 5000;
+        //consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight = 0;
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000000000000001d0c4d9cd");
@@ -402,17 +402,17 @@ public:
         consensus.nPowMaxAdjustDown = 0; // Turn off adjustment down
         consensus.nPowMaxAdjustUp = 0; // Turn off adjustment up
         consensus.nPowTargetSpacing = 2.5 * 60;
-        consensus.vUpgrades[Consensus::BASE_SPROUT].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::BASE_SPROUT].nActivationHeight =
+        //consensus.vUpgrades[Consensus::BASE_SPROUT].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::BASE_SPROUT].nActivationHeight =
             Consensus::NetworkUpgrade::ALWAYS_ACTIVE;
-        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
+        //consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
             Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight =
+        //consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight =
             Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-        consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight =
+        //consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 1;
+        //consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight =
             Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
 
         // The best chain should have at least this much work.
