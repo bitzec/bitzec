7bf58e78f050393d136c95094f4f128e97579bab
bitzec <bitzec18@gmail.com>
2018-09-22 12:51:04 +0200

    1

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -97,7 +97,7 @@ public:
         assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
         consensus.nPowMaxAdjustDown = 32; // 32% adjustment down
         consensus.nPowMaxAdjustUp = 16; // 16% adjustment up
-        consensus.nPowLwmaTargetSpacing = 2.5 * 60;
+        consensus.nPowTargetSpacing = 2.5 * 60;
         //  consensus.fPowNoRetargeting=false;
         //  consensus.nLWMAHeight=10000;
         //  consensus.nPowLwmaTargetSpacing = 1 * 60;
