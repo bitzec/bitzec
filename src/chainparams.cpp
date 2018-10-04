b5ac9d992f9f32703aba971893d4f43073614729
bitzec <bitzec18@gmail.com>
2018-10-05 01:06:14 +0200

    vm cheeckpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -173,19 +173,19 @@ public:
          checkpointData = (CCheckpointData) {
              boost::assign::map_list_of
              (0, consensus.hashGenesisBlock),
-             //(1, uint256S("0x000231e51c9d1102695c03b42125cd205c0532eb75a0ecab45764ec103ad69dc"))
-             //(2, uint256S("0x0004208b283a7c352e7855e3cc52e37bb520e1cbe37eea00ade1e0887a2074dd"))
-             //(3, uint256S("0x00070dfdd2857942e5b8b4fc3a4a12e6ff03ecd56cc561d659c7385da2532704")),
+             (1, uint256S("0x0006a8bd88af974b497811b98d8a08e9c09d7ccd2b6f3138666703f75fb32a94"))
+             (2, uint256S("0x000722840aa4e300bdc015410060a0e605d255591ee0bd349ffad8db0e204daf"))
+             (3, uint256S("0x0002a61ca5d0ae495f1719ad7ecf93ba5689d9f9058a73c79007cfe0ef3fa2ee")),
             //(100000, uint256S("0x000000001c5c82cd6baccfc0879e3830fd50d5ede17fa2c37a9a253c610eb285"))
             //(133337, uint256S("0x0000000002776ccfaf06cc19857accf3e20c01965282f916b8a886e3e4a05be9"))
             //(180000, uint256S("0x000000001205b742eac4a1b3959635bdf8aeada078d6a996df89740f7b54351d"))
             //(222222, uint256S("0x000000000cafb9e56445a6cabc8057b57ee6fcc709e7adbfa195e5c7fac61343"))
             //(270000, uint256S("0x00000000025c1cfa0258e33ab050aaa9338a3d4aaa3eb41defefc887779a9729"))
             //(304600, uint256S("0x00000000028324e022a45014c4a4dc51e95d41e6bceb6ad554c5b65d5cea3ea5")),
-            //1538655157,     // * UNIX timestamp of last checkpoint block
-            //4,        // * total number of transactions between genesis and last checkpoint
+            1538692720,     // * UNIX timestamp of last checkpoint block
+            4,        // * total number of transactions between genesis and last checkpoint
                             //   (the tx=... number in the SetBestChain debug.log lines)
-            //1000            // * estimated number of transactions per day after checkpoint
+            1000            // * estimated number of transactions per day after checkpoint
                             //   total number of tx / (checkpoint block height / (24 * 24))
         };
 
