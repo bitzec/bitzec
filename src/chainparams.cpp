314e0e2e5c8b0d25feeff561a9e8eeca85b5bc08
bitzec <bitzec18@gmail.com>
2018-10-09 14:04:53 +0200

    overwinter

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -103,7 +103,7 @@ public:
         consensus.nPowMaxAdjustUp = 16; // 16% adjustment up
         consensus.nPowTargetSpacing =2.5 * 60;
         consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 1;
-        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight = ;
+        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight =  
           Consensus::NetworkUpgrade::ALWAYS_ACTIVE;
         consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nProtocolVersion = 175001;
         consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight = 35000;
